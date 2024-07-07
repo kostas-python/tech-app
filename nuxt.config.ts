522fd769d6ffc274ad2763ca5a60690c6292b50c
Kostas <diamantopoulos.kostas@hotmail.com>
2024-07-07 19:20:37 +0100

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,50 +4,54 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 
 export default defineNuxtConfig({
-  vite: {
-    resolve: {
-      alias: {
-        '@heroicons/vue': '@heroicons/vue'
-      }
-    },
-    vue: {
-      template: {
-        transformAssetUrls,
+    vite: {
+      resolve: {
+        alias: {
+          '@heroicons/vue': '@heroicons/vue'
+        }
+      },
+      vue: {
+        template: {
+          transformAssetUrls,
+        },
       },
     },
-  },
-  
-  devtools: { enabled: true },
-  compatibilityDate: "2024-07-04",
-  modules: [
-    '@nuxtjs/tailwindcss','@element-plus/nuxt',
-    '@vueuse/nuxt', '@vueuse/motion/nuxt',
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
-    },
-  ],
+    
+      devtools: { enabled: true },
+      compatibilityDate: "2024-07-04",
+      modules: [
+        '@nuxtjs/tailwindcss','@element-plus/nuxt',
+        '@vueuse/nuxt', '@vueuse/motion/nuxt',
+        (_options, nuxt) => {
+          nuxt.hooks.hook('vite:extendConfig', (config) => {
+            // @ts-expect-error
+            config.plugins.push(vuetify({ autoImport: true }))
+          })
+        },
+      ],
 
-  tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-    configPath: '~/tailwind.config.js',
-    exposeConfig: false,
-    config: {},
-    injectPosition: 0,
-    viewer: true
-  },
-  css: [
-    '~/assets/css/tailwind.css', '@/assets/element-variables.scss'
-  ],
-  plugins: [
-    '@/plugins/element-plus.ts'
-  ],
-  build: {
-    transpile: ['@vueuse/motion', 'vuetify'],
-  },
-  vueuse: {
-    ssrHandlers: true
-  }
+      tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: '~/tailwind.config.js',
+        exposeConfig: false,
+        config: {},
+        injectPosition: 0,
+        viewer: true
+      },
+      
+      css: [
+        '~/assets/css/tailwind.css', '@/assets/element-variables.scss'
+      ],
+
+      plugins: [
+        '@/plugins/element-plus.ts'
+      ],
+
+      build: {
+        transpile: ['@vueuse/motion', 'vuetify'],
+      },
+
+      vueuse: {
+        ssrHandlers: true
+   }
 })
--- pages/vuetify.vue
+++ pages/vuetify.vue
@@ -1,65 +1,55 @@
-
 <template>
-  <div class="bg-white py-24 sm:py-32">
-    <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-      <div class="max-w-2xl">
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
-        <p class="mt-6 text-lg leading-8 text-gray-600">Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper suspendisse.</p>
-      </div>
-      <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
-        <li v-for="person in people" :key="person.name">
-          <div class="flex items-center gap-x-6">
-            <img class="h-16 w-16 rounded-full" :src="person.imageUrl" alt="" />
-            <div>
-              <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">{{ person.name }}</h3>
-              <p class="text-sm font-semibold leading-6 text-indigo-600">{{ person.role }}</p>
+  <div>
+    <v-hover v-slot="{ isHovering, props }">
+      <v-card
+        class="mx-auto"
+        color="grey-lighten-4"
+        max-width="600"
+        v-bind="props"
+      >
+        <v-img
+          :aspect-ratio="16/9"
+          src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
+          cover
+        >
+          <v-expand-transition>
+            <div
+              v-if="isHovering"
+              class="d-flex transition-fast-in-fast-out bg-orange-darken-2 v-card--reveal text-h2"
+              style="height: 100%;"
+            >
+              $14.99
             </div>
+          </v-expand-transition>
+        </v-img>
+
+        <v-card-text class="pt-6">
+          <div class="font-weight-light text-grey text-h6 mb-2">
+            For the perfect meal
           </div>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div class="bg-white py-24 sm:py-32">
-    <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-      <div class="max-w-2xl">
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
-        <p class="mt-6 text-lg leading-8 text-gray-600">Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper suspendisse.</p>
-      </div>
-      <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
-        <li v-for="person in people" :key="person.name">
-          <div class="flex items-center gap-x-6">
-            <img class="h-16 w-16 rounded-full" :src="person.imageUrl" alt="" />
-            <div>
-              <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">{{ person.name }}</h3>
-              <p class="text-sm font-semibold leading-6 text-indigo-600">{{ person.role }}</p>
-            </div>
+
+          <h3 class="text-h4 font-weight-light text-orange mb-2">
+            QW cooking utensils
+          </h3>
+
+          <div class="font-weight-light text-h6 mb-2">
+            Our Vintage kitchen utensils delight any chef.<br>
+            Made of bamboo by hand
           </div>
-        </li>
-      </ul>
-    </div>
+        </v-card-text>
+      </v-card>
+    </v-hover>
   </div>
 </template>
 
-<script setup>
-const people = [
-  {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  // More people...
-]
-</script>
+
+<style>
+  .v-card--reveal {
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    opacity: .9;
+    position: absolute;
+    width: 100%;
+  }
+</style>
